9104836dc5178b7b41ae7eab1ac908f3ea948f74
shree <srisajee19@gmail.com>
2025-04-28 10:03:54 +0530

    main changes

--- frontend/src/Pages/WorkoutStatus.jsx
+++ frontend/src/Pages/WorkoutStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";// Import React hooks
 import toast from "react-hot-toast";
 import axios from "axios";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
